944aef53fcd76c1829602ac3e8b3d5d9e3446c27
Aayush Mallya <aayushmallya72@gmail.com>
2020-09-24 22:56:07 -0400

    added print statement

---
+++ src/AddressBook.java
@@ -0,0 +1,13 @@
+public class AddressBook {
+
+    private void addBuddy(){}
+
+    private void removeBuddy(){}
+
+    public static void main(String[] args) {
+
+        System.out.println ("Address Book");
+    }
+
+}
+}
