2ccc83c200c3db4fd348c8da3100d1bc6fc0c1e3
Aayush Mallya <aayushmallya72@gmail.com>
2021-09-23 23:48:40 -0400

    Lab 3 Changes

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,23 +5,27 @@ public class AddressBook{
     private ArrayList<BuddyInfo> buddies;
 
     public AddressBook(){
-        this.buddies = new ArrayList<BuddyInfo>();
+
+        buddies = new ArrayList<BuddyInfo>();
     }
 
-    public void addBuddy(BuddyInfo buddy){
-        this.buddies.add(buddy);
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            this.buddies.add(buddy);
+        }
     }
-
-    public void removeBuddy(BuddyInfo buddy){
-        this.buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index) {
+        if (index >= 0 && index < buddies.size()) {
+            return buddies.remove(index);
+        }
+        return null;
     }
 
     public static void main(String [] args){
-        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton",  613);
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
-
+        addressBook.removeBuddy(0);
 
 
     }
